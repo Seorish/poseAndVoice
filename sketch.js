da3ccd46deed77e44da95b7d2c0e240744741e60
Seorish <seojin0774@naver.com>
2023-05-27 19:13:14 +0900

    인공지능공연기술과제제출

--- sketch.js
+++ sketch.js
@@ -8,14 +8,11 @@ let poseNet;
 let poses = [];
 
 let pkeyHandX, pkeyHandY, keyHandX, keyHandY;
-let img;
-function preload(){
-   img = loadImage("mm.jpg");
-}
+
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(700, 480);
   directionX = 1;
-  textSize(30);
+
   speechRec = new p5.SpeechRec("ko-KR", gotSpeech);
   let continuous = true;
   let interimResults = false;
@@ -51,22 +48,38 @@ function draw() {
 function drawKeypoints() {
   for (let i = 0; i < poses.length; i++) {
     let pose = poses[i].pose;
-    let keypoint = pose.keypoints[10];
+    let keypoint = pose.keypoints[9];
     if (keypoint.score > 0.2) {
       keyHandX = pkeyHandX;
       keyHandY = pkeyHandY;
       pkeyHandX = keypoint.position.x;
       pkeyHandY = keypoint.position.y;
 
-        if(keyHandX > width/2){
+      // if (keyHandX < keyHandX) {
+      if (keyHandX > width / 2) {
         directionX = -1;
       }
-      if(keyHandX < width/2){
+      if (keyHandX < width / 2) {
+        // if (keyHandX >= keyHandX) {
         directionX = 1;
       }
       for (var j = 0; j < word.length; j++) {
-        image(img, keyHandX-125, keyHandY, 250, 250);
-        text(word[j], keyHandX + directionX * j * 25, random(keyHandY - 3, keyHandY + 3));
+        text(
+          word[j],
+          keyHandX + directionX * j * 10,
+          random(keyHandY - 3, keyHandY + 3)
+        );
+        console.log(word[j]);
+      }
+
+      textSize(20); // 텍스트 크기 설정
+
+      for (var j = 0; j < word.length; j++) {
+        text(
+          word[j],
+          keyHandX + directionX * j * 10,
+          random(keyHandY - 3, keyHandY + 3)
+        );
         console.log(word[j]);
       }
     }
